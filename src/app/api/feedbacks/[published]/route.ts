ee60cdf1e1fbc0a225c438a80165a8fc9b0f6483
tahsin090 <tajisan789@gmail.com>
2025-10-11 23:50:25 +0600

    fixing the feedbacks route

--- src/app/api/feedbacks/[published]/route.ts
+++ src/app/api/feedbacks/[published]/route.ts
@@ -1,23 +1,24 @@
 import { connectDB } from "@/lib/connectDB";
 
 export const GET = async () => {
-    try {
-      const db = await connectDB();
-      const feedBackCollections = await db.collection("feedbacks");
-      const res = await feedBackCollections.find({ status: "published" }).toArray();
-      if (res) {
-        return Response.json({
-          status: 200,
-          data: res,
-        });
-      }
+  try {
+    const db = await connectDB();
+    const feedBackCollections = await db.collection("feedbacks");
+    const res = await feedBackCollections.find({ status: "published" }).toArray();
+    if (res) {
       return Response.json({
-        status: 404,
-        message: "No data Found",
-      });
-    } catch (error) {
-      return Response.json({
-        message: error,
+        status: 200,
+        data: res,
       });
     }
-  };
+    console.log(res)
+    return Response.json({
+      status: 404,
+      message: "No data Found",
+    });
+  } catch (error) {
+    return Response.json({
+      message: error,
+    });
+  }
+};
